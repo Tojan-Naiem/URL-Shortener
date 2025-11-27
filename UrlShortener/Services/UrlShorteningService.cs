fd26feb0a35480b031ab8982d57f73157dcfa34d
Tojan-Naiem <tojan050@gmail.com>
2025-11-27 15:48:20 +0200

    feat : add urlShortener service

--- UrlShortener/Services/UrlShorteningService.cs
+++ UrlShortener/Services/UrlShorteningService.cs
@@ -7,5 +7,17 @@ namespace UrlShortener.Services
         private const int NumberOfCharsInShortLink = 7;
         private const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
+        private readonly Random _random = new();
+        public string GenerateUniqueCode()
+        {
+            var codeChars = new char[NumberOfCharsInShortLink];
+            for(var i = 0; i < NumberOfCharsInShortLink; i++)
+            {
+                int randomIndex = _random.Next(Alphabet.Length - 1);
+                codeChars[i] = Alphabet[randomIndex];
+            }
+            var code= new string(codeChars);
+            return new string(codeChars);
+        }
     }
 }
