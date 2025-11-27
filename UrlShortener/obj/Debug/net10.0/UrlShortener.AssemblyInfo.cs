856555416b42d10d0b27534234e09e210fec4d14
Tojan-Naiem <tojan050@gmail.com>
2025-11-27 18:41:52 +0200

    feat : add redis package

--- UrlShortener/obj/Debug/net10.0/UrlShortener.AssemblyInfo.cs
+++ UrlShortener/obj/Debug/net10.0/UrlShortener.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e5848a31825a2b1816491255197f652ebdcf88f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda4cca11e1689145c27970aa8f705c37118a567")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
